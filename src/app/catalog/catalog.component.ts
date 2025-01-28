68aadbcf49d6d0da1175b1e90b5034ad0dde7678
Nolan Hansen <11377973+tkma496@users.noreply.github.com>
2025-01-28 12:41:11 -0600

    Commit of initial changes

---
+++ src/app/catalog/catalog.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { IProduct } from './product.model';
+
+@Component({
+  selector: 'app-catalog',
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.css']
+})
+export class CatalogComponent {
+  product: IProduct;
+
+  constructor() {
+    this.product = {
+      id: 2,
+      description:
+        "A friendly robot head with two eyes and a smile -- great for domestic use.",
+      name: "Friendly Bot",
+      imageName: "head-friendly.png",
+      category: "Heads",
+      price: 945.0,
+      discount: 0.2
+    };
+  }
+}
