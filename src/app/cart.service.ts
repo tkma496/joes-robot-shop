bc8a88a1a58c7788cb1c6fcdb3c27cbb84a2ab46
Nolan Hansen <11377973+tkma496@users.noreply.github.com>
2025-01-31 12:44:36 -0600

    Chapter 8 completed

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './catalog/product.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +9,12 @@ import { IProduct } from './catalog/product.model';
 export class CartService {
   cart: IProduct[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   add(product: IProduct) {
-      this.cart.push(product);
-      console.log(`product ${product.name} added to cart`);
-    }
+    this.cart.push(product);
+    this.http.post('/api/cart', this.cart).subscribe(() => {
+     console.log(`product ${product.name} added to cart`);
+    });
+  }
 }
